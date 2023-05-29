46c70b2fcf1c80621a3e6da4f6a5b42799a32148
Eszter Barbuscia <gubiscom@yahoo.com>
2023-05-29 14:33:13 -0700

    created renderLicenseLink function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -34,7 +34,25 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license===licenseArr[0]){
+    return `(https://opensource.org/licenses/MIT)`;
+  } else if (license===licenseArr[1]){
+      return `(https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license===licenseArr[2]){
+      return `http://unlicense.org`;
+  } else if (license===licenseArr[3]){
+      return `https://www.gnu.org/licenses/agpl-3.0)`;
+  } else if (license===licenseArr[4]){
+      return `https://www.gnu.org/licenses/lgpl-3.0)`;
+  } else if (license===licenseArr[5]){
+      return `https://opensource.org/licenses/MPL-2.0`;
+  } else if (license===licenseArr[6]){
+      return `https://opensource.org/licenses/Apache-2.0`;
+  } else if (license===licenseArr[7]){
+      return `https://www.boost.org/LICENSE_1_0.txt)`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
